0843fa017e94a80daee9b666a2cd6b5eee69fea5
Matt Kinzler <matthew.kinzler@gmail.com>
2015-12-04 21:56:25 -0500

    add comments

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,21 @@
 def encrypt(words,idx)
+	# create an alphabet array
 	alphabet = ('a'..'z').to_a
+	# create an empty array to append new letters
 	new_arr = []
+	# downcase and split input word into array of letters and iterate over each letter
 	words_arr = words.downcase.split("").map do |ltr|	
+		# if the letter is in the alphabet, do this operation
 		if ltr.match(/[a-z]/)
+			# append the transformed letter using addition of alphabet array's index and the supplied index, mod 26
 			new_arr << alphabet[(alphabet.index(ltr)+idx) % 26]
+		# if the letter is not in the alphabet, do this operation
 		else
+			# append the item to new_arr
 			new_arr << ltr 
 		end
 	end
+	# join and print out the characters in new_arr
 	puts new_arr.join
 end
 
